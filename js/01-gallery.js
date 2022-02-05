d4a59deae0a44e5a26ba3c3d276fdd577869440b
kbilyi <71833120+kbilyi@users.noreply.github.com>
2022-02-05 20:37:52 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// const galleryCardMarkup = document.querySelector('.gallery')
+// galleryContainer.insertAjacentHTML('beforeend', galleryCardMarkup(galleryItems))
+
+// function galleryCardMarkup(img) {
+//     return img.map(({ preview, original, description}) => {
+//         return `div class="gallery__item"`
+//     })
+// }
+
+// console.log(galleryItems);
+
+// const markup = galleryItems.map(item =>
+//     `<div class="gallery__item">
+//     <a class="gallery__link" href="${item.original}">
+//       <img
+//         class="gallery__image"
+//         src='${item.preview}'
+//         data-source='${item.original}'
+//         alt='${item.description}'
+//       />
+//     </a>
+//   </div>`).join('');
+
+//   let image = '';
+// function createImgInModal(e) {
+//   image = basicLightbox.create(`
+//     <img src=${e.target.dataset.source}>`);
+// }
+
+// console.log(galleryItems);
