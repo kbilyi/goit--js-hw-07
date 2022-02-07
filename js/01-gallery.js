2a877a50749de7cd4cc03b242f193c7f213b0014
kbilyi <71833120+kbilyi@users.noreply.github.com>
2022-02-07 17:06:59 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,82 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
+const gallaryEl = document.querySelector(".gallery");
+const createImageMarkupList = createImageMarkup(galleryItems);
 
-// function onImageToCreateModal(e) {
-//     e.preventDefault()
+gallaryEl.insertAdjacentHTML("afterbegin", createImageMarkupList);
 
-//     const selectedImage = e.target.dataset.source
-//     console.log(selectedImage)
-    
-//     const modal = basicLightbox.create(`
-//         <img src="${selectedImage}" width="800" height="600">
-//     `)
+gallaryEl.addEventListener("click", onLinkClick);
 
-//     modal.show()
-
-//     document.addEventListener('keydown', closeModal)
-    
-//     function closeModal(e) {
-//         if(event.code === "Escape") {
-//             document.querySelector(".gallery__image").style.display = 'none'
-//         }
-//     }
-// }
-
-// ===============================
-
-const createGallery = document.querySelector('.gallery')
-
-const allGallaryEl = galleryItems.map(
-    ({ preview, original, description }) => 
-        `<li class = 'gallery__item'>
-        <a class = 'gallery__link' href = "${original}">
-        <img class = 'gallery__image' src = "${preview}"
-        alt = "${description}"/>
-        </a></li>
-        `
-    )
-        .join('');
-
-    createGallery.insertAdjacentHTML('beforeend', allGallaryEl);
-
-    let gallery = new basicLigthbox('.gallery a', {
-        captionsData: 'alt',
-        captionPosition: 'bottom',
-        captionDelay: 250,
-    });
-
-// ================================
-
-
-const galleryCardMarkup = document.querySelector('.gallery')
-galleryContainer.insertAjacentHTML('beforeend', galleryCardMarkup(galleryItems))
-
-function galleryCardMarkup(img) {
-    return img.map(({ preview, original, description}) => {
-        return `div class="gallery__item"`
+function createImageMarkup(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+      <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src='${preview}'
+          data-source='${original}'
+          alt='${description}'
+        />
+      </a>
+    </div>`;
     })
+    .join("");
 }
 
+function onLinkClick(e) {
+  e.target.firstElementChild.preventDefault();
 
+  if (!e.target.classList.contains("gallary__item")) {
+    return;
+  }
 
-const markup = galleryItems.map(item =>
-    `<div class="gallery__item">
-    <a class="gallery__link" href="${item.original}">
-      <img
-        class="gallery__image"
-        src='${item.preview}'
-        data-source='${item.original}'
-        alt='${item.description}'
-      />
-    </a>
-  </div>`).join('');
+  const bigImageLink = e.target.firstElementChild.href;
 
-  let image = '';
-function createImgInModal(e) {
-  image = basicLightbox.create(`
-    <img src=${e.target.dataset.source}>`);
+  const intance = basicLightbox.create(
+    `<img src ="${bigImageLink}" width="800" height="600">`
+  );
 }
+
+// intance.show();
+// console.log(bigImageLink);
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,51 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
-const createGallery = document.querySelector('.gallery')
+const galleryEl = document.querySelector(".gallery");
 
-const allGallaryEl = galleryItems.map(
-    ({ preview, original, description }) => 
-        `<li class = 'gallery__item'>
-        <a class = 'gallery__link' href = "${original}">
-        <img class = 'gallery__image' src = "${preview}"
-        alt = "${description}"/>
-        </a></li>
-        `
-    )
-        .join('');
+const lighthBoxMarkup = (item) => {
+  const { description, original, preview } = item;
 
-    createGallery.insertAdjacentHTML('beforeend', allGallaryEl);
+  return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
+};
 
-    let gallery = new simpleLigthbox('.gallery a', {
-        captionsData: 'alt',
-        captionPosition: 'bottom',
-        captionDelay: 250,
-        captionSelector:'img',
-    });
+const lightBoxGalleryInserted = galleryItems.map(lighthBoxMarkup).join("");
 
+const handler = (event) => {
+  event.preventDefault();
+  let gallery = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  return gallery;
+};
 
-
-    function onImageToCreateModal(e) {
-    e.preventDefault()
-
-    const selectedImage = e.target.dataset.source
-    console.log(selectedImage)
-    
-    const modal = simpleLightbox.create(`
-        <img src="${selectedImage}" width="800" height="600">
-    `)
-
-    modal.show()
-
-    document.addEventListener('keydown', closeModal)
-    
-    function closeModal(e) {
-        if(e.code === "Escape") {
-            document.querySelector(".gallery__image").style.display = 'none'
-        }
-    }
-}
-
-onImageToCreateModal.show()
+galleryEl.insertAdjacentHTML('beforeend', lightBoxGalleryInserted)
+galleryEl.addEventListener('click', handler)
