0948aa2ad0360936bcae0cdee54ee3584fbfcbe9
kbilyi <71833120+kbilyi@users.noreply.github.com>
2022-02-07 13:44:07 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,33 +3,80 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
-// const galleryCardMarkup = document.querySelector('.gallery')
-// galleryContainer.insertAjacentHTML('beforeend', galleryCardMarkup(galleryItems))
 
-// function galleryCardMarkup(img) {
-//     return img.map(({ preview, original, description}) => {
-//         return `div class="gallery__item"`
-//     })
+// function onImageToCreateModal(e) {
+//     e.preventDefault()
+
+//     const selectedImage = e.target.dataset.source
+//     console.log(selectedImage)
+    
+//     const modal = basicLightbox.create(`
+//         <img src="${selectedImage}" width="800" height="600">
+//     `)
+
+//     modal.show()
+
+//     document.addEventListener('keydown', closeModal)
+    
+//     function closeModal(e) {
+//         if(event.code === "Escape") {
+//             document.querySelector(".gallery__image").style.display = 'none'
+//         }
+//     }
 // }
 
-// console.log(galleryItems);
+// ===============================
 
-// const markup = galleryItems.map(item =>
-//     `<div class="gallery__item">
-//     <a class="gallery__link" href="${item.original}">
-//       <img
-//         class="gallery__image"
-//         src='${item.preview}'
-//         data-source='${item.original}'
-//         alt='${item.description}'
-//       />
-//     </a>
-//   </div>`).join('');
+const createGallery = document.querySelector('.gallery')
 
-//   let image = '';
-// function createImgInModal(e) {
-//   image = basicLightbox.create(`
-//     <img src=${e.target.dataset.source}>`);
-// }
+const allGallaryEl = galleryItems.map(
+    ({ preview, original, description }) => 
+        `<li class = 'gallery__item'>
+        <a class = 'gallery__link' href = "${original}">
+        <img class = 'gallery__image' src = "${preview}"
+        alt = "${description}"/>
+        </a></li>
+        `
+    )
+        .join('');
 
-// console.log(galleryItems);
+    createGallery.insertAdjacentHTML('beforeend', allGallaryEl);
+
+    let gallery = new basicLigthbox('.gallery a', {
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+    });
+
+// ================================
+
+
+const galleryCardMarkup = document.querySelector('.gallery')
+galleryContainer.insertAjacentHTML('beforeend', galleryCardMarkup(galleryItems))
+
+function galleryCardMarkup(img) {
+    return img.map(({ preview, original, description}) => {
+        return `div class="gallery__item"`
+    })
+}
+
+
+
+const markup = galleryItems.map(item =>
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src='${item.preview}'
+        data-source='${item.original}'
+        alt='${item.description}'
+      />
+    </a>
+  </div>`).join('');
+
+  let image = '';
+function createImgInModal(e) {
+  image = basicLightbox.create(`
+    <img src=${e.target.dataset.source}>`);
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,50 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const createGallery = document.querySelector('.gallery')
+
+const allGallaryEl = galleryItems.map(
+    ({ preview, original, description }) => 
+        `<li class = 'gallery__item'>
+        <a class = 'gallery__link' href = "${original}">
+        <img class = 'gallery__image' src = "${preview}"
+        alt = "${description}"/>
+        </a></li>
+        `
+    )
+        .join('');
+
+    createGallery.insertAdjacentHTML('beforeend', allGallaryEl);
+
+    let gallery = new simpleLigthbox('.gallery a', {
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+        captionSelector:'img',
+    });
+
+
+
+    function onImageToCreateModal(e) {
+    e.preventDefault()
+
+    const selectedImage = e.target.dataset.source
+    console.log(selectedImage)
+    
+    const modal = simpleLightbox.create(`
+        <img src="${selectedImage}" width="800" height="600">
+    `)
+
+    modal.show()
+
+    document.addEventListener('keydown', closeModal)
+    
+    function closeModal(e) {
+        if(e.code === "Escape") {
+            document.querySelector(".gallery__image").style.display = 'none'
+        }
+    }
+}
+
+onImageToCreateModal.show()
